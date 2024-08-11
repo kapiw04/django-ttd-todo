817aa281b19bdf74408ca59bdd06b34817b7a00e
kapiw04 <wojtowicz.kacper@gmail.com>
2024-08-11 19:21:26 +0200

    Added end2end tests for all CRUD operations

--- todo_list/tests/functional_tests.py
+++ todo_list/tests/functional_tests.py
@@ -5,15 +5,18 @@ from selenium.webdriver.remote.webelement import WebElement
 import pytest
 
 @pytest.mark.django_db(transaction=True)
-def test_add_todo():
+def test_crud_todo():
   with webdriver.Firefox() as browser:
     browser.get("http://localhost:8000")
 
+    assert "To-Do List" in browser.title
+
+
+    #* create
     sample_todo = {
       "title": "Sample To-Do",
       "description": "This is a sample to-do item."
     }
-
     browser.find_element(By.ID, "open-add-button").click()
     browser.implicitly_wait(5)
     input_title = browser.find_element(By.ID, "id_title")
@@ -24,11 +27,49 @@ def test_add_todo():
     input_desc.send_keys(sample_todo["description"])
 
     browser.find_element(By.ID, "confirm-add-button").click()
+
+    # check if todo modal is no longer visible
+    with pytest.raises(Exception):
+      browser.find_element(By.ID, "id_title")
+
     todos_table = browser.find_element(By.ID, "todos-table")
     rows: List[WebElement] = todos_table.find_elements(By.TAG_NAME, "tr")
 
+    #* read
     assert todos_table is not None and rows is not None
-
     assert sample_todo["title"] in rows[-1].text
-  # browser.close(); pytest.fail("Finish Test")  # noqa: E702
+
+    #* update
+    updated_todo = {
+      "title": "Updated To-Do",
+      "description": "This was a sample to-do. Now it's updated."
+    }
+    update_button = rows[-1].find_element(By.ID, "update-button")
+    update_button.click()
+    browser.implicitly_wait(5)
+    input_title = browser.find_element(By.ID, "id_title")
+    input_desc = browser.find_element(By.ID, "id_desc")
+
+    # check if previous values are stored
+    assert input_title == sample_todo["title"]
+    assert input_desc == sample_todo["description"]
+
+    input_title.clear()
+    input_desc.clear()
+    pytest.fail("Finish Test")
+
+    input_title.send_keys(updated_todo["title"])
+    input_desc.send_keys(updated_todo["description"])
+
+    browser.find_element(By.ID, "confirm-update-button").click()
+
+    rows = todos_table.find_elements(By.TAG_NAME, "tr")
+    assert updated_todo["title"] in rows[-1].text
+
+    #* delete
+    rows_count = len(rows)
+    delete_button = rows[-1].find_element(By.ID, "delete-button")
+    delete_button.click()
+    rows = todos_table.find_elements(By.TAG_NAME, "tr")
+    assert rows_count - 1 == len(rows)
 
--- todo_list/tests/unit_tests.py
+++ todo_list/tests/unit_tests.py
@@ -1,11 +1,2 @@
-import pytest 
-
-@pytest.mark.django_db
-def test_home_page(client):
-  response = client.get("/")    
-  assert response.status_code == 200
-  assert "<title>To-Do List</title>" in str(response.content)
-    
-
 
 
