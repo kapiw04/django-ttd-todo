7d88b57c9c24150bbf3fe552ec44eb8852b00470
kapiw04 <wojtowicz.kacper@gmail.com>
2024-08-11 13:38:39 +0200

    Tested filling form to add a task

--- todo_list/tests/functional_tests.py
+++ todo_list/tests/functional_tests.py
@@ -2,6 +2,7 @@ from typing import List
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+import pytest
 
 def test_add_todo():
   browser = webdriver.Firefox()
@@ -12,17 +13,19 @@ def test_add_todo():
     "description": "This is a sample to-do item."
   }
 
-  browser.find_element(By.ID, "open_add_button").click()
+  browser.find_element(By.ID, "open-add-button").click()
   browser.implicitly_wait(5)
-  input_title = browser.find_element(By.ID, "input_title")
-  input_desc = browser.find_element(By.ID, "input_desc")
+  input_title = browser.find_element(By.ID, "input-title")
+  input_desc = browser.find_element(By.ID, "input-desc")
   
   input_title.send_keys(sample_todo["title"])
   input_desc.send_keys(sample_todo["description"])
 
-  browser.find_element(By.ID, "confirm_add_button").click()
+  browser.find_element(By.ID, "confirm-add-button").click()
 
-  todos_table = browser.find_element(By.ID, "todos_table")
+  todos_table = browser.find_element(By.ID, "todos-table")
   rows: List[WebElement] = todos_table.find_elements(By.TAG_NAME, "tr")
+  browser.close(); pytest.fail("Finish Test")  # noqa: E702
 
   assert sample_todo["title"] in rows[-1].text
+
